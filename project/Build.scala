b6767d603983e45f3ca3f0a0621824f913940599
Jerzy Zagorski <jerzy.zagorski@sabre.com>
2013-05-28 13:41:48 +0200

    black

--- project/Build.scala
+++ project/Build.scala
@@ -4,22 +4,22 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "git_training"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "git_training"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
     anorm
   )
-  
+
   def customLessEntryPoints(base: File): PathFinder = (
     (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
-    (base / "app" / "assets" / "stylesheets" * "*.less"))
+      (base / "app" / "assets" / "stylesheets" * "*.less"))
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    lessEntryPoints <<= baseDirectory(customLessEntryPoints))      
-  
+  val main = play.Project(appName, appVersion, appDependencies)
+    .settings(lessEntryPoints <<= baseDirectory(customLessEntryPoints))
+
 
 }
